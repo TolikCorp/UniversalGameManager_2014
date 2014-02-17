6859095addc5ef350666c361922d02915a899645
TolikCorp <TolikCorp@users.noreply.github.com>
2014-02-17 12:06:03 +0700

    Create ugm_server_csgo_conf.sh

---
+++ configs/ugm_server_csgo_conf.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+####################################################
+    date_day_month_year=`date +%F`
+    date_hours_minutes_seconds=`date +%T`
+    ilf="[---------------------------------------------------]"
+    ilh="[---------"
+    echo "${ilf}"
+    echo "${ilh} UniversalGameManager 2014 [CONFIG] [17.02.2014]"
+    echo "${ilh} ${date_day_month_year} ${date_hours_minutes_seconds}"
+    echo "${ilf}"
+
+# Физический IP-Адрес Выделенного сервера
+####################################################
+    server_ip_address="xxx.xxx.xxx.xxx"
+
+# Пользователь сервера
+####################################################
+    server_user="game_server"
+
+# AppID выделенного сервера
+####################################################
+#Counter-Strike: Global Offensive dedicated server  740
+#Garry's Mod dedicated server                       4020
+#Insurgency dedicated server                        17705
+#Left 4 Dead 2 dedicated server                     222860
+#Team Fortress 2 dedicated server                   232250
+#Day of Defeat: Source dedicated server             232290
+#Counter-Strike: Source dedicated server            232330
+#Half-Life 2: Deathmatch dedicated server           232370
+
+# Расположение дистрибутивов и хостинг-каталогов
+####################################################
+
+# Физическое расположение сервера
+    server_dir="/home/usert/csgo_1"
+# Название окна с сервером    
+    server_screen_title="csgo_1"
+# APP ID Dedicated Server
+    server_app_id="740"
+# Название игры для srcds_run
+    server_run_title="csgo"
+    
+# Параметры запуска
+####################################################
+# Tickrate
+    tickrate="66"
+# Игровой порт
+    server_game_port="27015"
+# Стартовая карта
+    default_map="de_dust2"
+# Количество игроков
+    maxplayers="25"
+# Дополнительные параметры запуска
+    params_ext="-nobots -nocrashdialog +tv_enable 0"
+    
+# Конфигурация автоматического обновления
+####################################################
+    case ${1} in
+        autoupdate)
+            autoupdate="-autoupdate -steam_dir ${2} -steamcmd_script ${3}"
+        ;;
+    esac
+    if [ -z "${autoupdate}" ]; then
+        autoupdate=""
+    fi
+    
+# Заполнение дополнительных параметров
+####################################################
+    # client_port и tv_port заполняются автоматически
+    client_port="$(( ${game_port} - 7000 ))"
+    tv_port="$(( ${game_port} - 8000 ))"
+    steam_port="$(( $game_port - 9000 ))"
+    
+# Запуск игрового сервера
+####################################################
+    while true; do
+        cd $server_dir
+        ./srcds_run -console -usercon -game ${game_name_run} -tickrate ${tickrate} -secure +game_type 0 +game_mode 0 +mapgroup mg_bomb +ip ${ip_address} +map ${default_map} -port ${game_port} +clientport ${client_port} +tv_port ${tv_port} -steamport ${steam_port} -maxplayers_override ${maxplayers} ${params_ext} $autoupdate
+        echo "${ilh} $(date +'%F-%R:%S') | Перезапуск сервера!" >> ./restarts.log
+        echo "Ожидайте 10 секунд"
+        sleep 10
+    done 
